c0201d8072273c9029179dc670e33c47005bf1e0
Jai <jaix2793@gmail.com>
2025-02-25 17:52:47 +0530

    updates

---
+++ src/app/contact/page.js
@@ -0,0 +1,5 @@
+import ContactUs from "@/components/Contact";
+
+export default function Page() {
+    return <ContactUs />
+}
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,10 +1,109 @@
-import ProductsPage from "@/components/home/Products";
+// import ProductsPage from "@/components/home/Products";
+import Products from "@/components/Products";
 
 
-export default  function page(){
-    return(<>
-    <ProductsPage/>
+const data = [
+    {
+        category: "Office Furniture",
+        banner: "/officeFurniture.jpg",
+        products: [
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shaped desk workstation with glass panels",
+                img: "/officeFurniture/L-ShapedDeskWorkStationWithGlass.png",
+            },
+            {
+                title: "Electric height adjustable standing desk",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "Straight desk with modesty panels & Fabric Partition",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "Wardrobe with / without glass doors",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "Wardrobe with / without glass doors",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+        ]
+    },
 
+    {
+        category: "Networking Products",
+        banner: "/officeFurniture.jpg",
+        products: [
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shaped desk workstation with glass panels",
+                img: "/officeFurniture/L-ShapedDeskWorkStationWithGlass.png",
+            },
+            {
+                title: "Electric height adjustable standing desk",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "Straight desk with modesty panels & Fabric Partition",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "Wardrobe with / without glass doors",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "Wardrobe with / without glass doors",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+            {
+                title: "L-Shape desks with mobile pedestal",
+                img: "/officeFurniture/LShapeWithMobilePedestale.png",
+            },
+        ]
+    },
+    {
+        category: "HVAC",
+        banner: "/"
+    }
+];
 
+export default function page() {
+    return (<>
+        <Products data={data} />
     </>)
 }
